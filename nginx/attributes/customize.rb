c2791f86c3b48dd9fe96efc56a6328cedc2c768f
中野賀通 <nakano@temona.co.jp>
2015-08-07 11:56:08 +0900

    add nginx unicorn custom

--- nginx/attributes/customize.rb
+++
@@ -1,3 +0,0 @@
-normal[:nginx][:client_max_body_size] = "10m"
-normal[:nginx][:proxy_send_timeou] = "300"
-normal[:nginx][:worker_processes] = node[:cpu][:total]
---
+++ nginx_unicorn_custom/recipes/default.rb
@@ -0,0 +1,50 @@
+# setup Unicorn service per app
+node[:deploy].each do |application, deploy|
+
+  deploy = node[:deploy][application]
+
+  execute "restart Rails app #{application}" do
+    cwd deploy[:current_path]
+    command node[:opsworks][:rails_stack][:restart_command]
+    action :nothing
+  end
+
+  template "#{deploy[:deploy_to]}/shared/config/unicorn.conf" do
+    mode '0644'
+    owner deploy[:user]
+    group deploy[:group]
+    source "unicorn.conf.erb"
+    variables(
+      :deploy => deploy,
+      :application => application,
+      :environment => OpsWorks::Escape.escape_double_quotes(deploy[:environment_variables])
+    )
+    notifies :run, "execute[restart Rails app #{application}]"
+
+  end
+
+  template "#{node[:nginx][:dir]}/sites-available/#{application}" do
+    mode '0644'
+    source "nginx_unicorn_web_app.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    notifies :restart, "service[nginx]"
+  end
+
+  # template "#{deploy[:deploy_to]}/shared/config/database.yml" do
+  #   source "database.yml.erb"
+  #   cookbook 'rails'
+  #   mode "0660"
+  #   group deploy[:group]
+  #   owner deploy[:user]
+  #   variables(:database => deploy[:database], :environment => deploy[:rails_env])
+
+  #   notifies :run, "execute[restart Rails app #{application}]"
+
+  #   only_if do
+  #     deploy[:database][:host].present? && File.directory?("#{deploy[:deploy_to]}/shared/config/")
+  #   end
+  # end
+
+end
