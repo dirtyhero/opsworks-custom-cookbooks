1c357f5345c5b6b8128fac26dc92cc78121c7cf8
中野賀通 <nakano@temona.co.jp>
2015-08-23 17:35:13 +0700

    no message

---
+++ export-envs/recipes/default.rb
@@ -0,0 +1,9 @@
+node.deploy.each_pair do |app_name,vals|
+  file File.join(vals[:home], "shellinit-#{app_name}.sh") do
+    envs = []
+    vals.environment_variables.each_pair {|key, val| envs << "export #{key}=\"#{val}\""} if vals.environment_variables
+    owner vals[:user]
+    group vals[:group]
+    content envs.join("\n")
+  end
+end
