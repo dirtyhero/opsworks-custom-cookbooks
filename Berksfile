2fb9ea3a79d6488ad179573156312549be3dc0d5
中野賀通 <nakano@temona.co.jp>
2015-09-07 16:11:05 +0700

    elastic search,kibanaを追加

--- Berksfile
+++ Berksfile
@@ -1,13 +1,22 @@
 source "https://supermarket.chef.io"
 
 metadata
-cookbook 'postfix'
-cookbook 'dovecot'
-cookbook 'user_management'
-cookbook 'yum'
-cookbook 'htpasswd'
-cookbook 'nginx_basic_auth', path: 'nginx_basic_auth'
-cookbook 'unicorn', path: 'unicorn'
-cookbook 'node_dump', path: 'node_dump'
-cookbook 'export-envs', path: 'export-envs'
-cookbook 'td-agent-custom',path: 'td-agent-custom'
+cookbook "postfix"
+cookbook "dovecot"
+cookbook "user_management"
+cookbook "yum"
+cookbook "htpasswd"
+cookbook "nginx_basic_auth", path: "nginx_basic_auth"
+cookbook "unicorn", path: "unicorn"
+cookbook "node_dump", path: "node_dump"
+cookbook "export-envs", path: "export-envs"
+cookbook "td-agent-custom",path: "td-agent-custom"
+cookbook "nodejs"
+cookbook "build-essential"
+cookbook "ark"
+cookbook "git"
+cookbook "nginx"
+cookbook "java"
+cookbook "elasticsearch"
+cookbook "kibana", git: "https://github.com/realityforge/chef-kibana.git"
+
---
+++ elasticsearch_plugins/recipes/default.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: elasticsearch_plugins
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+script "install_plugin_es_head" do
+  interpreter "bash"
+  user "root"
+  cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
+  code <<-EOH
+    plugin -install mobz/elasticsearch-head
+    EOH
+  not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/head") }
+end
+
+script "install_plugin_es_hq" do
+  interpreter "bash"
+  user "root"
+  cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
+  code <<-EOH
+    plugin -install royrusso/elasticsearch-HQ
+    EOH
+  not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/HQ") }
+end
+
+#notifies :restart, 'service[elasticsearch]' unless node.elasticsearch[:skip_restart]
