31d0a7ed2edebf0ce4ddfb66a61ba91f0ea591de
中野賀通 <nakano@temona.co.jp>
2015-09-11 11:00:23 +0700

    td-agentのconf.dにapplicationごとにsourceのtailの設定ファイルを追加

--- td-agent-custom/recipes/application.rb
+++ td-agent-custom/recipes/application.rb
@@ -10,4 +10,21 @@ node[:deploy].each do |application, deploy|
   service "td-agent" do
     action :nothing
   end
+
+  template "/etc/td-agent/conf.d/#{application}.conf" do
+    mode '0644'
+    source "td_agent_application.conf.erb"
+    owner 'td-agent'
+    group 'td-agent'
+    mode 0644
+    variables({
+      :deploy => deploy,
+      :application => application,
+      :environment => OpsWorks::Escape.escape_double_quotes(deploy[:environment_variables])
+    })
+    if File.exists?("#{node[:nginx][:dir]}/sites-enabled/#{application}")
+      notifies :reload, "service[td-agent]", :delayed
+    end
+  end
+
 end
