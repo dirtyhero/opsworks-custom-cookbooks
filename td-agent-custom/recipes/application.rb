d5180010f909f925ad2f3be7afa127263e78a27d
中野賀通 <nakano@temona.co.jp>
2015-09-11 10:00:55 +0700

    fix ) typo

--- td-agent-custom/recipes/application.rb
+++ td-agent-custom/recipes/application.rb
@@ -1,10 +1,10 @@
 node[:deploy].each do |application, deploy|
 
   Chef::Log.level = :debug
-  Chef::Log.debug("-----td-agent-custom application-----"
-  Chef::Log.debug("-----application-----"
+  Chef::Log.debug("-----td-agent-custom application-----")
+  Chef::Log.debug("-----application-----")
   Chef::Log.debug("#{application}")
-  Chef::Log.debug("-----deploy-----"
+  Chef::Log.debug("-----deploy-----")
   Chef::Log.debug("#{deploy}")
 
   service "td-agent" do
